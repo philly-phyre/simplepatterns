c28863b34547d39f31a882e7d184efd63605d33e
Phil C <philecarpenter@gmail.com>
2016-01-04 16:13:16 -0500

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+
+
+    public class Solution {
+
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();            
+            String ans="";
+            if(n%2==1 || n%2==0 && (n >= 6 && n <= 20)) { // nice, concise statements
+                ans = "Weird";
+            } else {
+                ans = "Not Weird";
+            }
+            System.out.println(ans);
+            
+        }
+    }
